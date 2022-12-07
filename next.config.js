0c3a283b55f41756c8f3423c9510dfc9b274da75
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-12-07 12:51:15 +0630

    setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			require("./scripts/sitemap-generator.js");
+		}
+		return config;
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
