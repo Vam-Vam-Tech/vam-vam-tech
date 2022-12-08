06ea1d4184bb8d56425dda7301264e02061f5bc9
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-12-08 12:50:12 +0630

    google analytics: implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,23 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Script
+				strategy="afterInteractive"
+				src="https://www.googletagmanager.com/gtag/js?id=G-BXCTQZL4K7"
+			/>
+			<Script
+				id="google-analytics"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BXCTQZL4K7', {
+          page_path: window.location.pathname,
+          });`,
+				}}
+			/>
 			<Script id="google-tag-manager" strategy="afterInteractive">
 				{`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
